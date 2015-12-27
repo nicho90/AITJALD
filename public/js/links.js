a5f7e482b52b1e7dbe8b8e5cc53cfd3d20d923d1
Nicho S <n.schiestel@uni-muenster.de>
2015-12-27 18:14:29 +0100

    Added Highcharts
    - map_demo_2.html and map_demo_3.html are only for demonstration

---
+++ public/js/links.js
@@ -0,0 +1,13 @@
+
+// LINKS (ONLY FOR MOCKUP)
+$('#level_1').click(function(){
+    document.location.href = "map.html";
+});
+
+$('#level_2').click(function(){
+    document.location.href = "map_demo_2.html";
+});
+
+$('#level_3').click(function(){
+    document.location.href = "map_demo_3.html";
+});
